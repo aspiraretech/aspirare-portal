b01c5383fc49882ef5523004e727dc6c19d9b6e2
aspiraretech <aspirare.tech@gmail.com>
2022-10-28 17:34:12 +0530

    minor change

--- dist/js/controllers/admin/addEventController.js
+++ dist/js/controllers/admin/addEventController.js
@@ -74,7 +74,7 @@ angular.module('app')
         };
 
         $scope.addImages = function() {
-            cloudinary.openUploadWidget({ cloud_name: 'dzerq05zm', upload_preset: 'findinbox' },
+            cloudinary.createUploadWidget({ cloud_name: 'diyrbgmir', upload_preset: 'aspirare' },
                 function(error, result) {
                     console.log(error, result)
                 });
